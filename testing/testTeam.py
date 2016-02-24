f2daa6a78e1367a81247b94cec496d050886906d
Marisela Del Valle <marisela.delvalle93@gmail.com>
2016-02-24 15:13:46 -0430

    Creando archivo para probar funcionalidades de Team.py

---
+++ testing/testTeam.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-. 
+
+import sys
+import unittest
+
+# Ruta que permite utilizar el módulo Team.py
+sys.path.append('../app/scrum')
+
+from team import *
+
+class TestTeam(unittest.TestCase):
