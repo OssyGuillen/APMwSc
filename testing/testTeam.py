6f31846e00b10830eceb8120c087a981feaffb4a
Marisela Del Valle <marisela.delvalle93@gmail.com>
2016-02-24 19:02:22 -0430

    Creando primera prueba para testear Team.py

--- testing/testTeam.py
+++ testing/testTeam.py
@@ -6,6 +6,20 @@ import unittest
 # Ruta que permite utilizar el módulo Team.py
 sys.path.append('../app/scrum')
 
-from team import *
+from Team import *
 
 class TestTeam(unittest.TestCase):
+	
+	#############################################      
+    #         Pruebas para emptyTable           #
+    #############################################
+
+    def testEmptyTable(self):
+    	# Probar que la funcionalidad se ejecuta
+    	team_object = team()
+    	team_object.emptyTable()
+    	
+
+    	
+if __name__ == '__main__':
+    unittest.main()
