0c0c4446102ab97edfa35e75ccf3a7e058a0a1ec
John <pexison@gmail.com>
2016-02-27 23:32:44 -0430

    testSuitWithoutEclipse.py created for the buildbot

---
+++ testing/testSuitWithoutEclipse.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-.
+
+import unittest
+
+from testAccions                import *
+from testActorsUserHistory      import *
+from testBackLog                import *
+from testCategory               import *
+from testHistory                import *
+from testLogin                  import *
+from testObjective              import *
+from testObjectivesUserHistory  import *
+from testRole                   import *
+from testTask                   import *
+from testUser                   import *
+from testArchivo                import *
+
+if __name__ == '__main__':
+    unittest.main()
+
