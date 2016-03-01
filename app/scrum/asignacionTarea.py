1a5ddca84653e3dd0bc5a2e59a8ba9897884499b
Marcos Campos <whosthemark@gmail.com>
2016-03-01 00:17:37 -0430

    Integrated new views Some actions between views don't work at the moment

---
+++ app/scrum/asignacionTarea.py
@@ -0,0 +1,65 @@
+from flask import request, session, Blueprint, json
+
+asignacionTarea = Blueprint('asignacionTarea', __name__)
+
+
+@asignacionTarea.route('/asignacionTarea/AActuralizarAsignacionTarea', methods=['POST'])
+def AActuralizarAsignacionTarea():
+    #POST/PUT parameters
+    params = request.get_json()
+    results = [{'label':'/VAsignacionTarea', 'msg':['Asignación actualizada']}, {'label':'/VAsignacionTarea', 'msg':['Error al actualizar la asignacion de la tarea']}, ]
+    res = results[0]
+    res['idTarea'] = 1
+    #Action code goes here, res should be a list with a label and a message
+
+
+    #Action code ends here
+    if "actor" in res:
+        if res['actor'] is None:
+            session.pop("actor", None)
+        else:
+            session['actor'] = res['actor']
+    return json.dumps(res)
+
+
+
+@asignacionTarea.route('/asignacionTarea/VAsignacionTarea')
+def VAsignacionTarea():
+    #GET parameter
+    idTarea = request.args['idTarea']
+    res = {}
+    if "actor" in session:
+        res['actor']=session['actor']
+    #Action code goes here, res should be a JSON structure
+
+    if 'usuario' not in session:
+      res['logout'] = '/'
+      return json.dumps(res)
+
+    res['usuario'] = session['usuario']
+    res['idPila'] = 1
+    res['idTarea'] = 1
+    res['fAsignacionTarea'] = {'lista':[
+        {'miembro':1},
+        {'miembro':2},
+        {'miembro':3},
+      ]}
+    res['fAsignacionTarea_opcionesMiembros'] =[
+        {'key':1, 'value':'Mia'},
+        {'key':2, 'value':'Mara'},
+        {'key':3, 'value':'Marcos'},
+        {'key':4, 'value':'Julia'},
+        {'key':5, 'value':'Roberto'},
+      ]
+    #Action code ends here
+    return json.dumps(res)
+
+
+
+
+
+#Use case code starts here
+
+
+#Use case code ends here
+
