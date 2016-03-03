8efcde9e692f83c8c8fa8a8f69957e5ebdd95bdc
08-11021 <globerusso@gmail.com>
2016-03-03 13:19:04 -0430

    se agrego la funcionalidad para subir archivos asociados a una tarea

--- app/scrum/tareas.py
+++ app/scrum/tareas.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
-
-from flask                 import request, session, Blueprint, json
+import os
+from flask                 import request, session, Blueprint, json, render_template, redirect
 from app.scrum.backLog     import *
 from app.scrum.userHistory import *
 from app.scrum.task        import *
+from werkzeug import secure_filename
 
 tareas = Blueprint('tareas', __name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 @tareas.route('/tareas/ACrearTarea', methods=['POST'])
@@ -14,7 +16,7 @@ def ACrearTarea():
     params  = request.get_json()
     results = [{'label':'/VHistoria', 'msg':['Tarea creada']}, {'label':'/VHistoria', 'msg':['No se pudo crear tarea.']}, ]
     res     = results[0]
-    
+
     # Obtenemos el id de la historia actual
     idHistory = int(session['idHistoria'])
 
@@ -24,10 +26,10 @@ def ACrearTarea():
     taskPeso    = params['peso']
     oBackLog    = backlog()
     oTask       = task()
-    
+
     insert   = oTask.insertTask(taskDesc, idCategoria, taskPeso, idHistory)
-    
-    if insert:        
+
+    if insert:
         res = results[0]
     else:
         res = results[1]
@@ -53,7 +55,7 @@ def AElimTarea():
     idHistoria = int(session['idHistoria'])
     idTarea    = int(session['idTarea'])
 
-    # Eliminamos la tarea 
+    # Eliminamos la tarea
     oTarea     = task()
     result     = clsTask.query.filter_by(HW_idTask = idTarea).first()
     delete     = oTarea.deleteTask(result.HW_description)
@@ -85,17 +87,17 @@ def AModifTarea():
     idTarea         = params['idTarea']
     new_idCategoria = params['categoria']
     new_taskPeso    = params['peso']
-  
+
     # Buscamos la tarea a modificar
     oTarea   = task()
     result   = clsTask.query.filter_by(HW_idTask = idTarea).first()
- 
+
     # Modificamos la tarea
     modify   = oTarea.updateTask(result.HW_description,new_description,new_idCategoria,new_taskPeso)
-    
+
     if modify:
         res = results[0]
-         
+
     res['label'] = res['label'] + '/' + str(idHistoria)
 
     if "actor" in res:
@@ -109,37 +111,37 @@ def AModifTarea():
 @tareas.route('/tareas/VCrearTarea')
 def VCrearTarea():
     #GET parameter
-    res = {}    
+    res = {}
     # Obtenemos el id de la historia actual
     idHistory = int(request.args.get('idHistoria'))
-    
+
     if "actor" in session:
         res['actor']=session['actor']
 
     if 'usuario' not in session:
       res['logout'] = '/'
       return json.dumps(res)
-  
+
     # Buscamos la historia actual
     oUserHistory = userHistory()
     hist         = oUserHistory.searchIdUserHistory(idHistory)
-    
+
     res['usuario']        = session['usuario']
     res['codHistoria']    = hist[0].UH_codeUserHistory
-    
+
     # Obtenemos una lista con los datos asociados a las categorías
-    cateList  = clsCategory.query.all()        
-    
+    cateList  = clsCategory.query.all()
+
     # Mostramos los datos en la vista
     ListaCompleta = []
     for i in cateList:
         ListaCompleta.append((i.C_idCategory,i.C_nameCate,i.C_weight))
-    
+
     decorated = [(tup[2], tup) for tup in ListaCompleta]
     decorated.sort()
 
     res['fTarea_opcionesCategoria'] = [
-     {'key':cat[1][0] ,'value':cat[1][1]+" ("+str(cat[1][2])+")",'peso':cat[1][2]} for cat in decorated]                                 
+     {'key':cat[1][0] ,'value':cat[1][1]+" ("+str(cat[1][2])+")",'peso':cat[1][2]} for cat in decorated]
 
     res['fTarea'] = {'idHistoria':idHistory}
 
@@ -152,14 +154,14 @@ def VCrearTarea():
 @tareas.route('/tareas/VTarea')
 def VTarea():
     #GET parameter
-    
+
     # Obtenemos el id de la historia y de la tarea
     idTarea    = int(request.args['idTarea'])
     idHistoria = int(session['idHistoria'])
 
     found = clsUserHistory.query.filter_by(UH_idUserHistory = idHistoria).first()
     codHistoria = found.UH_codeUserHistory
-    
+
     res = {}
     if "actor" in session:
         res['actor']=session['actor']
@@ -167,7 +169,7 @@ def VTarea():
     idTarea = request.args.get('idTarea')
     result   = clsTask.query.filter_by(HW_idTask = idTarea).first()
     categoryList     = clsCategory.query.all()
-    
+
     if 'usuario' not in session:
       res['logout'] = '/'
       return json.dumps(res)
@@ -187,8 +189,36 @@ def VTarea():
 
     return json.dumps(res)
 
+@tareas.route('/tareas/upload', methods=['POST'])
+def upload():
+    file = request.files['file']
+    idTask = request.form.get('idTarea')
+    docDescription = request.form.get('docDescription')
+    if file and allowed_file(file.filename):
+        docName = file.filename
+        docsDir=basedir+"/TaskDocuments"
+        dirName=docsDir+"/"+idTask
 
-#Use case code starts here
+        #verifica si existe la carpeta TaskDocuments, caso contrario la crea
+        if not(os.path.exists(docsDir)):
+            os.mkdir(docsDir)
+        #verifica si existe la carpeta especifica de la tarea, caso contrario la crea
+        if not(os.path.exists(dirName)):
+            os.mkdir(dirName)
+
+        file.save(os.path.join(dirName, docName))
+        try:
+            taskDoc = clsTaskDoc(idTask,docName,docDescription)
+            taskDoc.save()
+        except:
+            print("error al guardar el documento")
+        return redirect("#/VTarea/"+idTask)
+
+def allowed_file(filename):
+    return True#'.' in filename and \
+  #         filename.rsplit('.', 1)[1] in get_extensions()
+
+
 
 
 #Use case code ends here
