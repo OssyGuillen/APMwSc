0b210bc3ce6118de4dbf2ddc5fd0c008e6e31932
Andrea Diaz <andred242@gmail.com>
2016-03-02 23:48:48 -0430

    sprint

--- app/scrum/sprint.py
+++ app/scrum/sprint.py
@@ -1,4 +1,7 @@
 from flask import request, session, Blueprint, json
+from app.scrum.sprintClass       import *
+from app.scrum.meetingClass      import *
+from app.scrum.backLog           import *
 
 sprint = Blueprint('sprint', __name__)
 
@@ -23,18 +26,29 @@ def ACrearReunionSprint():
     return json.dumps(res)
 
 
-
 @sprint.route('/sprint/ACrearSprint', methods=['POST'])
 def ACrearSprint():
     #POST/PUT parameters
-    params = request.get_json()
-    results = [{'label':'/VSprints', 'msg':['Sprint creado']}, {'label':'/VSprint', 'msg':['Error al crear Sprint']}, ]
-    res = results[0]
-    #Action code goes here, res should be a list with a label and a message
+    params   = request.get_json()
+    results = [{'label':'/VSprints', 'msg':['Sprint creado']}, {'label':'/VCrearSprint', 'msg':['Error al crear Sprint']}, ]
+    res     = results[1]
 
-    res['label'] = res['label'] + '/' + repr(1)
+    # Obtenemos el id del producto
+    idPila  = int(session['idPila'])
 
-    #Action code ends here
+    if request.method == 'POST':
+        # Extraemos los parámetros
+        newNumero      = params['numero'] 
+        newDescription = params['descripcion']
+        
+        oSprint = sprints()
+        result  = oSprint.insertSprint(newNumero, newDescription, idPila)
+
+        if result:
+            res = results[0]
+
+    res['label'] = res['label'] + '/' + str(idPila)
+
     if "actor" in res:
         if res['actor'] is None:
             session.pop("actor", None)
@@ -43,16 +57,31 @@ def ACrearSprint():
     return json.dumps(res)
 
 
-
 @sprint.route('/sprint/AElimSprint')
 def AElimSprint():
     #POST/PUT parameters
-    params = request.get_json()
+    params  = request.get_json()
     results = [{'label':'/VSprints', 'msg':['Sprint eliminado']}, {'label':'/VSprint', 'msg':['Error al eliminar Sptrint']}, ]
-    res = results[0]
-    #Action code goes here, res should be a list with a label and a message
+    res     = results[1]
+    
+    # Obtenemos el id del producto
+    idPila       = int(session['idPila'])
 
-    res['label'] = res['label'] + '/' + repr(1)
+    # Obtenemos el id del sprint
+    #idSprint = int(params['idSprint'])
+    idSprint = int(session['idSprint'])
+
+    # Conseguimos el sprint a eliminar
+    oSprint  = sprint()
+    found    = oSprint.searchIdSprint(idSprint,idPila)
+
+    if (found != []):
+        deleted = oSprint.deleteSprint(found[0].S_numero, found[0].S_idBacklog)
+
+    if deleted:
+            res = results[0]  
+
+    res['label'] = res['label'] + '/' + str(idSprint)
 
     #Action code ends here
     if "actor" in res:
@@ -92,18 +121,32 @@ def AModifReunionSprint():
 def AModifSprint():
     #POST/PUT parameters
     params = request.get_json()
-    results = [{'label':'/VSprints', 'msg':['Sprint modificado']}, {'label':'/VSprint', 'msg':['Error al guardar Spront']}, ]
+
+    results = [{'label':'/VSprints', 'msg':['Sprint modificado']}, {'label':'/VSprints', 'msg':['Error al guardar Sprint']}, ]
     res = results[0]
-    #Action code goes here, res should be a list with a label and a message
 
-    res['label'] = res['label'] + '/' + repr(1)
+    idPila   = int(session['idPila'])
+    idSprint = int(session['idSprint'])
+    newSprintNumber = int(params['numero'])
+    newDescription  = str(params['descripcion'])
 
-    #Action code ends here
+    res['label'] = res['label'] + '/' + str(idPila)
+    oSprint = sprints()
+    result  = oSprint.updateSprint(idSprint, idPila, newSprintNumber, newDescription)
+    
+    if not result:
+        res = results[1]        
+        res['label'] = res['label'] + '/' + str(idPila)
+
     if "actor" in res:
         if res['actor'] is None:
             session.pop("actor", None)
         else:
             session['actor'] = res['actor']
+
+    res['idPila'] = idPila
+
+
     return json.dumps(res)
 
 
@@ -132,20 +175,21 @@ def VCrearReunionSprint():
 @sprint.route('/sprint/VCrearSprint')
 def VCrearSprint():
     #GET parameter
-    idPila = request.args['idPila']
     res = {}
+
+    idPila = request.args.get('idPila',1)
+        
     if "actor" in session:
         res['actor']=session['actor']
-    #Action code goes here, res should be a JSON structure
 
     if 'usuario' not in session:
       res['logout'] = '/'
       return json.dumps(res)
+
+    res['fSprint'] = {'idPila':idPila}
     res['usuario'] = session['usuario']
-    res['fSprint'] = {'idPila':1}
+    res['idPila']  = idPila
 
-
-    #Action code ends here
     return json.dumps(res)
 
 
@@ -168,47 +212,61 @@ def VReunion():
 @sprint.route('/sprint/VSprint')
 def VSprint():
     #GET parameter
-    idPila = request.args['idPila']
     res = {}
+
+    # Obtenemos el id del producto y del sprint
+    idPila   = int(session['idPila'])
+    idSprint = int(request.args.get('idSprint',1))
+    
     if "actor" in session:
         res['actor']=session['actor']
-    #Action code goes here, res should be a JSON structure
 
     if 'usuario' not in session:
       res['logout'] = '/'
       return json.dumps(res)
     res['usuario'] = session['usuario']
-    res['fSprint'] = {
-      'idPila':1,
-      'numero':1, 
-      'descripcion' :'Carrera inicial. Modelo de datos, MVC, identificación'
-      }
-    res['idPila'] = 1
-    res['idSprint'] = 1
 
-    #Action code ends here
+    # Buscamos el actor actual
+    oSprint = sprints()
+    result  = oSprint.searchIdSprint(idSprint,idPila)
+    
+    res['fSprint'] = {'idSprint':idSprint, 'numero':result[0].S_numero, 'descripcion':result[0].S_sprintDescription} 
+
+    res['data1'] = [{'id':1, 'fecha':'hola', 'actividades':'holaq' }]   
+
+    session['idSprint'] = idSprint
+    res['idPila'] = idPila
+
+    
+
     return json.dumps(res)
-
-
+    
 
 @sprint.route('/sprint/VSprints')
 def VSprints():
     #GET parameter
-    idPila = request.args['idPila']
     res = {}
+    
+    # Obtenemos el id del producto.
+    idPila = int(request.args.get('idPila',1)) 
+
     if "actor" in session:
         res['actor']=session['actor']
-    #Action code goes here, res should be a JSON structure
 
     if 'usuario' not in session:
       res['logout'] = '/'
       return json.dumps(res)
-    res['usuario'] = session['usuario']
-    res['data1'] = [{'numero':1, 'descripcion':'Carrera inicial'}]
-    res['idPila'] = 1
-    res['idSprint'] = 1
 
-    #Action code ends here
+    res['usuario'] = session['usuario']
+
+    oBacklog   = backlog()
+    sprintList = oBacklog.sprintsAsociatedToProduct(idPila)
+    res['data1'] = [{'numero':spr.S_numero, 'descripcion':spr.S_sprintDescription } for spr in sprintList]
+
+
+    session['idPila'] = idPila
+    res['idPila']     = idPila 
+ 
     return json.dumps(res)
 
 
