bc93eccf3042a68c3a41f2b2e0fb83371e1e049f
Carlos Farinha <09-10270@usb.ve>
2016-03-02 23:28:51 -0430

    Update testTarea_Trompo.py

--- testing/testTarea_Trompo.py
+++ testing/testTarea_Trompo.py
@@ -16,8 +16,8 @@ from archivos import *
 class TestArchivo(unittest.TestCase):
 
 
- #############################################
-    #         Pruebas para VAsignacionTarea        #
+    #############################################
+    #         Pruebas para VAsignacionTarea     #
     #############################################
 
     # Caso inicial
@@ -42,6 +42,61 @@ class TestArchivo(unittest.TestCase):
            return json.dumps(res)
        result = VAsignacionTarea(idTarea)
        self.assertFalse(result)
+
+
+    #############################################
+    #  Pruebas para AActualizarAsignacionTarea  #
+    #############################################
+
+    # Caso inicial
+    #Verificar que la actualizacion a asignar se lleve a cabo de manera correcta
+    # Prueba 3
+    params = request.get_json()
+    results = [{'label':'/VAsignacionTarea', 'msg':['Asignación actualizada']}, {'label':'/VAsignacionTarea', 'msg':['Error al actualizar la asignacion de la tarea']}, ]
+    res = results[0]
+    #Inicio de la Prueba
+    idTarea  = int(session['idTarea'])
+    idTarea2  = int(session['idTarea2'])
+    res['label'] = res['label'] + '/' + str(idTarea)
+    c = task()
+    c.insertUserTask(idTarea, 4)
+    c.insertUserTask(idTarea2, 3)
+    VAsignacionTarea(idTarea)
+    result = AActualizarAsignacionTarea(idTarea2)
+    self.assertTrue(result)
+    
+    # Caso Normal
+    #Verificar que la actualizacion de cambiar la asignacion al mismo usuario
+    # Prueba 4
+    params = request.get_json()
+    results = [{'label':'/VAsignacionTarea', 'msg':['Asignación actualizada']}, {'label':'/VAsignacionTarea', 'msg':['Error al actualizar la asignacion de la tarea']}, ]
+    res = results[0]
+    #Inicio de la Prueba
+    idTarea  = int(session['idTarea'])
+    res['label'] = res['label'] + '/' + str(idTarea)
+    c = task()
+    c.insertUserTask(idTarea, 4)
+    VAsignacionTarea(idTarea)
+    result = AActualizarAsignacionTarea(idTarea)
+    self.assertTrue(result)
+    
+    # Caso por Malicia
+    #Verificar que la actualizacion de cambiar la asignacion a un usuario nulo
+    # Prueba 5
+    params = request.get_json()
+    results = [{'label':'/VAsignacionTarea', 'msg':['Asignación actualizada']}, {'label':'/VAsignacionTarea', 'msg':['Error al actualizar la asignacion de la tarea']}, ]
+    res = results[0]
+    #Inicio de la Prueba
+    idTarea  = int(session['idTarea'])
+    idTarea2  = int(session[None])
+    res['label'] = res['label'] + '/' + str(idTarea)
+    c = task()
+    c.insertUserTask(idTarea, 1)
+    VAsignacionTarea(idTarea)
+    result = AActualizarAsignacionTarea(idTarea2)
+    self.assertFalse(result)
+ 
+    
 
 #if __name__ == '__main__':
     #unittest.main()
