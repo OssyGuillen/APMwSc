6989046069745e7c8477c123c9bfa6abc8cdc0e8
Yue <yue@localhost.localdomain>
2016-03-03 20:24:03 -0430

    Asign member in drop down interface test

--- testing/testInterfaz_Trompo.py
+++ testing/testInterfaz_Trompo.py
@@ -1,3 +1,4 @@
+# -*- encoding: utf-8 -*
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from time                           import sleep
@@ -7,6 +8,25 @@ browser = webdriver.Firefox()
 browser.get('http://0.0.0.0:5000')
 assert 'scrum' in browser.title
 
+# browser.find_element_by_xpath("(//a[contains(text(),'Registrarse')])").click()
+# nombre_completo = browser.find_element_by_id('fUsuario_nombre')
+# nombre_completo.send_keys('Reinaldo Verdugo')
+
+# usuario = browser.find_element_by_id('fUsuario_usuario')
+# usuario.send_keys('reiverdugo')
+
+# usuario_clave = browser.find_element_by_id('fUsuario_clave')
+# usuario_clave.send_keys('ve@7IVlv')
+
+# usuario_clave = browser.find_element_by_id('fUsuario_clave2')
+# usuario_clave.send_keys('ve@7IVlv')
+
+# usuario_correo = browser.find_element_by_id('fUsuario_correo')
+# usuario_correo.send_keys('verdugoreinaldo@gmail.com')
+
+# browser.find_element_by_xpath("//select[@id='fUsuario_actorScrum']/option[@value='2']").click()
+# browser.find_element_by_xpath("//button[@type='submit']").click()
+# sleep(2)
 user = browser.find_element_by_id('fLogin_usuario')
 user.send_keys('reiverdugo')
 password = browser.find_element_by_id('fLogin_clave')
@@ -24,15 +44,16 @@ sleep(2)
 browser.find_element_by_xpath("(//a[contains(text(),'+Tarea')])").click()
 sleep(2)
 descripcion = browser.find_element_by_id('fTarea_descripcion')
-descripcion.send_keys('Para el diseñador de la aplicacion')
+descripcion.send_keys('Para el desarrollador de la aplicacion')
 
 browser.find_element_by_xpath("//select[@id='fTarea_categoria']/option[@value='1']").click()
 sleep(2)
 peso_tarea = browser.find_element_by_id('fTarea_peso')
 peso_tarea.send_keys(Keys.RETURN)
 sleep(2)
-browser.find_element_by_xpath("(//a[contains(text(),'Detalles')])[2]").click()
+browser.find_element_by_xpath("(//a[contains(text(),'Detalles')])[1]").click()
 sleep(2)
-browser.find_element_by_xpath("(//a[contains(text(),'Asignación')])").click()
-assert browser.current_url == 'http://0.0.0.0:5000/#/VAsignacionTarea/5'
-# browser.quit()
+browser.find_element_by_xpath("//select[@id='fTarea_miembro']/option[@value='2']").click()
+sleep(2)
+browser.find_element_by_xpath("//button[@type='submit']").click()	
+assert browser.current_url == 'http://0.0.0.0:5000/#/VHistoria/7'
