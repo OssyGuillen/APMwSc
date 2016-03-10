a6255014e1e53b8992728a45cbd7d5e62d1dfeac
Daniel Pelayo <daniel.pelayo23@gmail.com>
2016-03-10 16:07:27 -0430

    arregladas pruebas meetingClass para sprint2

--- app/scrum/meetingClass.py
+++ app/scrum/meetingClass.py
@@ -51,14 +51,14 @@ class meeting(object):
 		
 		if checkTypeIdSprint:
 			foundMeeting =  self.getMeetings(idSprint)
-			#print("FOUND MEETING", foundMeeting)
+			print("FOUND MEETING", foundMeeting)
 			for m in foundMeeting:
 				dbDate = datetime.datetime.strptime(m.SM_meetingDate,'%d/%m/%Y')
 				#print (date,dbDate)
 				#print (date == dbDate)
 				if (date == dbDate):
 					new.append(m)
-					#print('remove')
+					print('remove')
 		return new
 
 	def insertMeeting(self, date, activities, suggestions, challenges, mtype, idSprint): 
