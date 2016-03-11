d3760be14b9e47e043651bd8be1aafc8cd055b43
Monica <monica.yuncosa@gmail.com>
2016-03-11 00:05:08 -0430

    Prueba de interfaz para Sprint 2

--- testing/testInterface-MostrarEHT.py
+++ testing/testInterface-MostrarEHT.py
@@ -6,14 +6,10 @@ from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
+
 from time import sleep
 
-import sys
-sys.path.append('../app/scrum')
-from backLog import *
-
-
-class TestInterfaceSprint(unittest.TestCase):
+class TestInterfaceMostrarEHT(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(30)
@@ -21,60 +17,69 @@ class TestInterfaceSprint(unittest.TestCase):
         self.verificationErrors = []
         self.accept_next_alert = True
     
-    def test_interface_sprint(self):
+    def test_interface_mostrar_e_h_t(self):
         driver = self.driver
-
-        everybacklog = clsBacklog.query.all()
-        number= len(everybacklog) + 1 
 
         # Usuario inicia sesión
         driver.get(self.base_url + "/#/VLogin")
+        sleep(1)
         driver.find_element_by_id("fLogin_usuario").clear()
         driver.find_element_by_id("fLogin_usuario").send_keys("ascander")
         sleep(1)
+
         driver.find_element_by_id("fLogin_clave").clear()
         driver.find_element_by_id("fLogin_clave").send_keys("C0ntr4sen4.")
         sleep(1)
 
         driver.find_element_by_xpath("//button[@type='submit']").click()
         sleep(2)
-
+        
         driver.find_element_by_link_text("Ver").click()
         sleep(2)
-        
+
         driver.find_element_by_link_text("Sprints").click()
         sleep(2)
 
         driver.find_element_by_link_text("Ver").click()
         sleep(2)
 
-        # Se modifica un sprint ya existente
-        driver.find_element_by_id("fSprint_descripcion").clear()
-        driver.find_element_by_id("fSprint_descripcion").send_keys("Sprint 1, Producto 1")
+        driver.find_element_by_link_text("+Asignar Historia").click()
+        sleep(2)
+
+        Select(driver.find_element_by_id("fSprintHistoria_historia")).select_by_visible_text("H1 Epica")
+        sleep(1)
         driver.find_element_by_xpath("//button[@type='submit']").click()
-        sleep(3)        
-        
-        # Se crea un nuevo sprint
-        driver.find_element_by_link_text("+Sprint").click()
-        sleep(2)
+        sleep(4)
 
-        driver.find_element_by_id("fSprint_numero").clear()
-        driver.find_element_by_id("fSprint_numero").send_keys("15")
-        sleep(2)
+        # Se asigna una nueva tarea al primer sprint de la lista
+        #driver.find_element_by_link_text("+Asignar Tarea").click()
+        #sleep(2)
+        #Select(driver.find_element_by_id("fSprintTarea_tarea")).select_by_visible_text("Tarea 1 H2")
+        #sleep(1)
+        #driver.find_element_by_xpath("//button[@type='submit']").click()
+        #sleep(2)
 
-        driver.find_element_by_id("fSprint_descripcion").clear()
-        driver.find_element_by_id("fSprint_descripcion").send_keys("Sprint 3 Producto 1")
-        sleep(2)
-
-        driver.find_element_by_id("fSprint_numero").clear()
-        driver.find_element_by_id("fSprint_numero").send_keys("14")
-        sleep(2)
-
-        driver.find_element_by_id("fSprint_descripcion").clear()
-        driver.find_element_by_id("fSprint_descripcion").send_keys("Sprint 3, Producto 1")
+        # Se guarda el sprint modificado
         driver.find_element_by_xpath("//button[@type='submit']").click()
         sleep(3)
-    
+
+        # Ver segundo Sprint de la lista agrega una historia
+        driver.find_element_by_xpath("(//a[contains(text(),'Ver')])[2]").click()
+        sleep(2)
+        driver.find_element_by_link_text("+Asignar Historia").click()
+        sleep(2)
+
+        Select(driver.find_element_by_id("fSprintHistoria_historia")).select_by_visible_text("H3")
+        sleep(1)
+        driver.find_element_by_xpath("//button[@type='submit']").click()
+        sleep(3)
+
+        # Se guarda el sprint modificado
+        driver.find_element_by_xpath("//button[@type='submit']").click()
+        sleep(2)
+        driver.find_element_by_xpath("(//a[contains(text(),'Ver')])[2]").click()
+        sleep(3)
+
     def is_element_present(self, how, what):
         try: self.driver.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
