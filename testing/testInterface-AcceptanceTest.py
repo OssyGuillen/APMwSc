d74c14741a387f036773b3efc1c073355983dc58
Reinaldo Verdugo <verdugoreinaldo@gmail.com>
2016-03-11 03:50:57 -0800

    Completed interface tests with selenium

--- testing/testInterface-AcceptanceTest.py
+++ testing/testInterface-AcceptanceTest.py
@@ -6,8 +6,9 @@ from time                           import sleep
 browser = webdriver.Firefox()
 
 browser.get('http://0.0.0.0:5000')
-assert 'scrum' in browser.title
 
+assert 'scrum' in browser.title
+## Login
 user = browser.find_element_by_id('fLogin_usuario')
 user.send_keys('marcos')
 password = browser.find_element_by_id('fLogin_clave')
@@ -16,6 +17,7 @@ from time                           import sleep
 assert browser.current_url == 'http://0.0.0.0:5000/#/VProductos'
 sleep(2)
 
+## Create acceptance test
 browser.find_element_by_xpath("(//a[contains(text(),'Ver')])[1]").click()
 sleep(2)
 browser.find_element_by_xpath("(//a[contains(text(),'Historias')])").click()
@@ -28,9 +30,29 @@ description = browser.find_element_by_id('fPrueba_descripcion')
 description.send_keys('Ejemplo de descripcion en prueba de aceptacion')
 sleep(2)
 fileUpload = browser.find_element_by_id('fPrueba_contenido')
-fileUpload.send_keys("/home/reinaldoverdugo/Documents/Software/APMwSc/testing/testInterface-AcceptanceTest.py")
+fileUpload.send_keys("/home/marcos/soft2/venv3/APMwSc/testing/testInterface-AcceptanceTest.py"")
 browser.find_element_by_xpath("//button[@type='submit']").click()
 sleep(2)
+
+## Delete acceptance test
 browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 sleep(2)
+browser.find_element_by_xpath("(//a[contains(text(),'-prueba')])[1]").click()
+browser.execute_script("window.scrollTo(0, 0);")
+sleep(2)
 
+## Create another acceptance test
+browser.find_element_by_xpath("(//a[contains(text(),'+Prueba')])").click()
+sleep(2)
+description = browser.find_element_by_id('fPrueba_descripcion')
+description.send_keys('Segundo ejemplo de descripcion en prueba de aceptacion')
+sleep(2)
+fileUpload = browser.find_element_by_id('fPrueba_contenido')
+fileUpload.send_keys("/home/marcos/soft2/venv3/APMwSc/testing/testInterface-AcceptanceTest.py")
+browser.find_element_by_xpath("//button[@type='submit']").click()
+sleep(2)
+
+## Download script
+browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+sleep(3)
+browser.find_element_by_xpath("(//a[contains(text(),'Descargar script')])[1]").click()
