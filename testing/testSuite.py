91d5cb33af95c278dd83ccddf0bed285f1291d94
Andres <ahernandez@jjbcompu-innovation.com>
2016-03-10 23:49:18 -0430

    Pyunit complete e incomplete

--- testing/testSuite.py
+++ testing/testSuite.py
@@ -13,6 +13,7 @@ from testObjectivesUserHistory  import *
 from testRole                   import *
 from testTask                   import *
 from testUser                   import *
+from testUserHistory 			import *
 
 if __name__ == '__main__':
     runner = unittest.TextTestRunner()
