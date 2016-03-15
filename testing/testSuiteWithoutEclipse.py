06d4b3895b0609bd455ed4c8ba654a4eb5f38dc9
Jonnathan Ng <ngjonnathan@gmail.com>
2016-03-15 14:00:59 -0430

    Arreglos BD, ahora solamente el dueño del producto es el que puede agregar historias a un sprint

--- testing/testSuiteWithoutEclipse.py
+++ testing/testSuiteWithoutEclipse.py
@@ -7,6 +7,7 @@ os.chdir("testing")
 
 from testAccions                import *
 from testActorsUserHistory      import *
+from testArchivo                import *
 from testBackLog                import *
 from testCategory               import *
 from testHistory                import *
@@ -14,9 +15,10 @@ from testLogin                  import *
 from testObjective              import *
 from testObjectivesUserHistory  import *
 from testRole                   import *
+from testSprint                 import *
 from testTask                   import *
+from testTeam                   import *
 from testUser                   import *
-from testArchivo                import *
 
 if __name__ == '__main__':
     unittest.main()
