68417673f68c1e82245d0f56b486b01bcb21c94d
“Patricia“ <patricia.reinoso12@gmail.com>
2016-03-17 21:46:45 -0430

    test de todos para  bulbul

---
+++ testing/testSuiteWithoutEclipse.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-.
+
+import unittest
+import os
+
+os.chdir("testing")
+
+from testAccions                import *
+from testActorsUserHistory      import *
+from testArchivo                import *
+from testBackLog                import *
+from testCategory               import *
+from testElementMeeting			import *
+from testHistory                import *
+from testLogin                  import *
+from testMeeting 				import *
+from testObjective              import *
+from testObjectivesUserHistory  import *
+#from testPrueba                 import *
+from testRole                   import *
+from testSprint                 import *
+from testTask                   import *
+from testTeam                   import *
+from testUser                   import *
+
+if __name__ == '__main__':
+    unittest.main()
+
